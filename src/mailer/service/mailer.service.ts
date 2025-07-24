969c28294683f8d1b3b9ac342b20f9be56d4a38a
Samimalik2030 <msamiullah2030@gmail.com>
2025-07-24 06:28:51 +0500

    mailer module implemented

--- src/mailer/service/mailer.service.ts
+++ src/mailer/service/mailer.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import * as nodemailer from 'nodemailer';
 import * as path from 'path'; // for safe file path resolution
+import * as fs from 'fs';
 
 @Injectable()
 export class MailerService {
@@ -8,10 +9,11 @@ export class MailerService {
 
   constructor() {
     this.transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: 'live.smtp.mailtrap.io',
+      port: 587,
       auth: {
-        user: process.env.GMAIL_USERNAME,
-        pass: process.env.GMAIL_PASSWORD,
+        user: 'api',
+        pass: process.env.MAILTRAP_API_TOKEN,
       },
     });
   }
@@ -22,32 +24,30 @@ export class MailerService {
     userName: string,
     message: string,
   ) {
+    // Read image file and convert to Base64
+    const imagePath = path.join(process.cwd(), 'src', 'common', 'logo.png');
+    const imageData = fs.readFileSync(imagePath);
+    const base64Image = imageData.toString('base64');
+    const mimeType = 'image/png'; // or 'image/png' depending on your logo
+
     const htmlContent = `
       <div style="font-family: Arial, sans-serif; padding: 20px; color: #333;">
         <div style="text-align: center;">
-          <img src="cid:logo" alt="Company Logo" style="width: 100px; margin-bottom: 20px;" />
+          <img src="data:${mimeType};base64,${base64Image}" alt="Company Logo" style="width: 100px; margin-bottom: 20px;" />
         </div>
         <h2 style="color: #2c3e50;">Hello, ${userName}!</h2>
         <p>${message}</p>
         <p>If you have any questions, feel free to contact us.</p>
         <hr style="margin: 40px 0;" />
-        <p style="font-size: 12px; color: #888;">This email was sent by Professional Service, Lahore, Pakistan</p>
+        <p style="font-size: 12px; color: #888;">This email was sent by Zaroorat, Multan, Pakistan</p>
       </div>
     `;
 
     const mailOptions = {
-      from: `"Professional Service" <${process.env.GMAIL_USERNAME}>`,
+      from: '"Zaroorat" <no-reply@www.zaroorat.com.pk>',
       to,
       subject,
       html: htmlContent,
-      attachments: [
-        {
-          filename: 'logo.png',
-          path: path.join(process.cwd(), 'src', 'common', 'logo.jfif'),
-
-          cid: 'logo',
-        },
-      ],
     };
 
     try {
